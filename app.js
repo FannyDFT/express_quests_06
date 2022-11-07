b839fabc7dcb386aa95076818685eb1d8b752748
FannyDFT <fannyd.erfurth@yahoo.fr>
2022-11-07 14:39:44 +0100

    express_quests_02

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
+require("dotenv").config();
+const port = process.env.APP_PORT ?? 5000;
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+app.use(express.json());
 
+//Affiche "Welcome ... " dans le serveur
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+const movieHandlers = require("./movieHandlers"); //Importe le fichier "movieHandler"
+const usersHandler = require("./usersHandler"); // Import du fichier "userHandlers"
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+// Méthode GET (movies)
+app.get("/api/movies", movieHandlers.getMovies); //Affiche les différents films
+app.get("/api/movies/:id", movieHandlers.getMovieById); // Sélectionne un film en particulier
+
+//Métode GET (users)
+app.get("/api/users", usersHandler.getUsers);
+app.get("/api/users/:id", usersHandler.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
